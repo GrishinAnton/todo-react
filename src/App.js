00c461262c45710d83430cb0f93163a85afaa75f
Grishin Anton <grishin_anton@mail.ru>
2018-06-26 17:56:38 +0300

    Настройка разметки

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React, { Component } from 'react';
-import logo from 'img/logo.svg';
+import React from 'react';
+import moment from 'moment';
+import Todo from './containers';
 import 'css/App.css';
-import moment from 'moment'
 
-class App extends Component {
-  render() {
-    console.log(moment().format());
-    
+export default function App() {    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Todo />
     );
-  }
 }
 
-export default App;
+
---
+++ src/containers/index.js
@@ -0,0 +1,4 @@
+import React from "react";
+import Todo from "containers/todo";
+
+export default () => <Todo />;
