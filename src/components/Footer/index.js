4a9a84ae617ada1b3126ce29159f2d4f858d69ca
Grishin Anton <grishin_anton@mail.ru>
2018-06-27 16:10:20 +0300

    Окончание верстки

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,27 +1,80 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export default function Footer () {
+export default function Footer() {
 
     return (
-        <footer className="footer">
-            <div className="footer-count">
+        <Footers>
+            <FooterCount className="footer-count">
                 <p><span>0</span> задач</p>
-            </div>
-            <ul className="footer-panel">
-                <li>
+            </FooterCount>
+            <FooterList className="footer-panel">
+                <FooterItem>
                     <button>Все</button>
-                </li>
-                <li>
+                </FooterItem>
+                <FooterItem>
                     <button>Активные</button>
-                </li>
-                <li>
+                </FooterItem>
+                <FooterItem>
                     <button>Завершенные</button>
-                </li>
-            </ul>
-            <div className="footer-count">
-                <p>Удалить выполненные задачи</p>
-            </div>
-        </footer>
+                </FooterItem>
+            </FooterList>
+            <FooterTask className="footer-task-delete">
+                <button>Удалить выполненные задачи</button>
+            </FooterTask>
+        </Footers>
     )
 } 
+
+const Footers = styled.footer `
+    display: flex;
+    justify-content: space-between;
+    align-items: center;    
+`;
+
+const FooterCount = styled.div `
+    p {
+        color: #777;
+        font-size: 14px;
+    }
+`;
+
+const FooterList = styled.ul `
+    display: flex;
+
+`;
+
+const FooterItem = styled.li `
+
+    button {
+        padding: 3px;       
+        color: #777;
+        font-size: 14px;
+        background-color: transparent;
+        border: 1px solid transparent;
+        margin-right: 15px; 
+
+        transition: all .2s;
+
+        &:hover {
+            border: 1px solid #2c2c2c;
+        }
+    }
+
+    &:last-child button {
+        margin-right: 0;
+    }
+`;
+
+const FooterTask = styled.div `
+
+    button {
+        padding: 0;
+
+        border: none;
+        background-color: transparent;
+        color: #777;
+        font-size: 14px;
+    }
+
+`;
