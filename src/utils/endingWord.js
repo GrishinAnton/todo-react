4a27b736772f42a38c2825b09611d12d7bd64c16
Grishin Anton <grishin_anton@mail.ru>
2018-06-28 11:36:20 +0300

    Добавил рабочий вариант окончания

--- src/utils/endingWord.js
+++ src/utils/endingWord.js
@@ -1,7 +1,6 @@
 export default function endingWord(number, one, endI, many){
-    
 
-    if ((number < 5 || (number % 10) < 5) && del !== 0) {     
+    if ((number < 5 || (number > 20 && (number % 10 < 5 && number % 10 !==0))) && number !== 0) {     
         var del = number % 10;  
         return del > 1 ? endI : one
     } else {
